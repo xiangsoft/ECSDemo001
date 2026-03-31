4bad24cfe8c6e04c76c490e2ee4f552afafd660b
xiangsoft <xiangsoft@hotmail.com>
2026-03-31 15:36:55 +0800

    加入了KCP2K和MemoryPack为了网络相关做准备

---
+++ Assets/Scripts/Network/NetworkMessages.cs
@@ -0,0 +1,27 @@
+﻿using MemoryPack;
+using System.Collections.Generic;
+
+namespace Xiangsoft.Game.Network
+{
+    [MemoryPackable]
+    public partial class PlayerCommand
+    {
+        public int PlayerID { get; set; }
+        public long MoveDirX { get; set; }
+        public long MoveDirZ { get; set; }
+        public bool IsCastSkill { get; set; }
+    }
+
+    [MemoryPackable]
+    public partial class FrameData
+    {
+        public int FrameID { get; set; }
+        public List<PlayerCommand> Commands { get; set; }
+
+        [MemoryPackConstructor]
+        public FrameData()
+        {
+            Commands = new List<PlayerCommand>();
+        }
+    }
+}

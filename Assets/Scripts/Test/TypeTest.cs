673d61a50c698408d4b319c5e78999984c891205
xiangsoft <xiangsoft@hotmail.com>
2026-04-18 15:42:02 +0800

    添加了部分测试代码

---
+++ Assets/Scripts/Test/TypeTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Xiangsoft.Game.Test
+{
+    public class TestPerson
+    {
+        public string Name { get; set; }
+    }
+
+    public class TypeTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            TestPerson person = new TestPerson { Name = "张三" };
+
+            string str = "你好啊";
+            doSomethingString(str);
+            doSomethingPerson(person);
+            Debug.Log(str);
+            Debug.Log(person.Name);
+        }
+
+        private void doSomethingString(string str)
+        {
+            str = "哈哈哈";
+        }
+
+        private void doSomethingPerson(TestPerson person)
+        {
+            person.Name = "李四";
+        }
+    }
+}
